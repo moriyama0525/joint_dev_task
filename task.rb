0f38c9d969959e8ac61d419ada54739555a49b8b
takamasamoriyama <ymca0525a@gmail.com>
2020-02-16 23:16:11 +0900

    add Q16~Q20

--- task.rb
+++ task.rb
@@ -55,7 +55,7 @@ def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
-  p array.map!(&:upcase)
+  p array.map!(|x| x.to_i)
 
 end
 
@@ -63,7 +63,7 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-  programming_languages.map!(&:upcase)
+  programming_languages.map!(&:capitalize)
   upper_case_programming_languages = programming_languages.map(&:upcase)
 
   # 以下は変更しないで下さい
@@ -85,11 +85,8 @@ def q10
 
   # 以下に回答を記載
   foods.each do |food|
-    if food.include?("うに")
-      puts "好物です"
-    else
-      puts "まあまあ好きです"
-    end
+    food.include?("うに") ? puts "好物です" : puts "まあまあ好きです"
+  end
 end
 
 def q11
@@ -123,7 +120,7 @@ def q14
 
   # 以下に回答を記載
   data_keys = data.keys
-  puts data_keys
+  p data_keys
 end
 
 def q15
@@ -144,12 +141,28 @@ def q16
   ]
 
   # 以下に回答を記載
-
+  users.each do |user|
+    puts "私の名前は#{user[:name]}です。 年齢は#{user[:age]}歳です。"
+  end
 end
 
 class UserQ17
   # 以下に回答を記載
+  def initialize(user)
+    @name = user[:name]
+    @age = user[:age]
+    @gender = user[:gender]
+    @admin = user[:admin] ? "有り" : "無し"
+  end
 
+  def info
+    puts <<~TEXT
+      名前：#{@name}
+      年齢：#{@age}
+      性別：#{@gender}
+      管理者権限：#{@admin}
+    TEXT
+  end
 end
 
 def q17
@@ -164,7 +177,18 @@ end
 
 class UserQ18
   # 以下に回答を記載
+  def initialize(user)
+    @name = user[:name]
+    @age = user[:age]
+  end
 
+  def introduce
+    if @age < 15
+      puts "はいさいまいど～、#{@name}です！！！"
+    else
+      puts "こんにちは、#{@name}と申します。宜しくお願いいたします。"
+    end
+  end
 end
 
 def q18
@@ -178,6 +202,7 @@ end
 
 class Item
   # 以下を修正して下さい
+  attr_reader :name
 
   def initialize(name)
     @name = name
@@ -192,12 +217,39 @@ end
 
 class UserQ20
   # 以下に回答を記載
+  attr_reader :name, :age
 
+  def initialize(user)
+    @name = user[:name]
+    @age = user[:age]
+  end
 end
 
 class Zoo
   # 以下に回答を記載
+  def initialize(zoo)
+    @name = zoo[:name]
+    @infant = zoo[:entry_fee][:infant]
+    @children = zoo[:entry_fee][:children]
+    @adult = zoo[:entry_fee][:adult]
+    @senior = zoo[:entry_fee][:senior]
+  end
 
+  def info_entry_fee(user)
+    fee =
+    case user.age
+    when 0..5
+      @infant
+    when 6..12
+      @children
+    when 13..64
+      @adult
+    when 65..120
+      @senior
+    end
+
+    puts "#{user.name}さんの入場料は#{fee}です"
+  end
 end
 
 
