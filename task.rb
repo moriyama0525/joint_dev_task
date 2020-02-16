eb0610dcb334cf85a6a7720893f095c5a5371594
takamasamoriyama <ymca0525a@gmail.com>
2020-02-16 17:09:28 +0900

    add Q10~Q15

--- task.rb
+++ task.rb
@@ -6,7 +6,7 @@ def q1
 
   # 以下に回答を記載
   names << "斎藤"
-  puts names
+  p names
 end
 
 def q2
@@ -55,7 +55,7 @@ def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
-  p array.map!{|X| x.to_i}
+  p array.map!(&:upcase)
 
 end
 
@@ -63,8 +63,8 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-  programming_languages.map!{|x| x.capitalize}
-  upper_case_programming_languages = programming_languages.map{|l| l.upcase}
+  programming_languages.map!(&:upcase)
+  upper_case_programming_languages = programming_languages.map(&:upcase)
 
   # 以下は変更しないで下さい
   p programming_languages
@@ -84,21 +84,29 @@ def q10
   foods = %w(いか たこ うに しゃけ うにぎり うに軍艦 うに丼)
 
   # 以下に回答を記載
-
+  foods.each do |food|
+    if food.include?("うに")
+      puts "好物です"
+    else
+      puts "まあまあ好きです"
+    end
 end
 
 def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
-
+  puts "ユーザーの趣味一覧"
+  sports.flatten.uniq.each.with_index(1) do |sport, i|
+    puts "No.#{i} #{sport}"
+  end
 end
 
 def q12
   data = { user: { name: "satou", age: 33 } }
 
   # 以下に回答を記載
-
+  puts data[:user][:name]
 end
 
 def q13
@@ -106,14 +114,16 @@ def q13
   update_data = { age: 32, address: "沖縄" }
 
   # 以下に回答を記載
-
+  user_data.merge!(update_data)
+  puts user_data
 end
 
 def q14
   data = { name: "satou", age: 33, address: "saitama", hobby: "soccer", email: "hoge@fuga.com" }
 
   # 以下に回答を記載
-
+  data_keys = data.keys
+  puts data_keys
 end
 
 def q15
@@ -121,7 +131,8 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
-
+  p data1.has_key?(:age) ? "~>OK" : "~>NG"
+  p data2.has_key?(:age) ? "~>OK" : "~>NG"
 end
 
 def q16
